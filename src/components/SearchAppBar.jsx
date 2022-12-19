6faa0cfea42605b3fa1c809234d54c1f85ccb0ab
Josephine <josephine.closan@yahoo.com>
2022-12-19 14:19:31 +0200

    add navbar

--- src/components/SearchAppBar.jsx
+++ src/components/SearchAppBar.jsx
@@ -5,8 +5,8 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
-// import MenuIcon from '@mui/icons-material/Menu';
-// import SearchIcon from '@mui/icons-material/Search';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
@@ -62,22 +62,23 @@ export default function SearchAppBar() {
             aria-label="open drawer"
             sx={{ mr: 2 }}
           >
-            {/* <MenuIcon /> */}
+            <MenuIcon />
           </IconButton>
           <Typography
             variant="h6"
             noWrap
             component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            sx={{ flexGrow: 1, textAlign: "left", display: { xs: 'none', sm: 'block' } }}
           >
-            MUI
+            Country
           </Typography>
           <Search>
             <SearchIconWrapper>
-              {/* <SearchIcon /> */}
+              <SearchIcon />
             </SearchIconWrapper>
             <StyledInputBase
-              placeholder="Search…"
+            id='search-input'
+              placeholder="Search by country name"
               inputProps={{ 'aria-label': 'search' }}
             />
           </Search>
